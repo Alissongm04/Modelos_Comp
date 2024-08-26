85dd462bbb1e83d5d329d42d0e1bb0fdb5319697
Alissongm04 <alissongarcia04@comunidad.unam.mx>
2024-08-26 16:37:50 -0600

    Agregando tarea de nuevo

---
+++ Mi repositorio/21 de agosto.py
@@ -0,0 +1,16 @@
+# list. Tipo compuesto de dato que puede almacenar distintos elementos
+#(llamados items) ordenado entre { } y separados con comas
+
+#ejemplo de lista
+list1 = {2, 4, 7}
+print(type(list1))
+
+#Ejemplo 2
+list2 = {2, -17/3, complex(3, 7.5), 5-7j, 'chain'}
+print('Lista list2 = ', list2)
+print('El tipo de dato de list2 es:', type(list2))
+
+#Ejemplo 3
+list3 = {'tomate', 'aguacate', 'papa'}
+list4 = {'lobo', 'jirafa', 'tigre', 5, 'oso'}
+list5 = {'ocho', 19.2, -4, 5+8,  }
