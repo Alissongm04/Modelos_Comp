9a9b2a12a8c53ebf31820989207486e6aad0a4ef
Alissongm04 <alissongarcia04@comunidad.unam.mx>
2024-09-17 19:07:25 -0600

    examen

--- pythonProject/Clases/clases_con_numpy/clase1.3.py
+++ pythonProject/Clases/clases_con_numpy/clase1.3.py
@@ -10,13 +10,13 @@ lista2 = [11, 12, 13, 14, 15]
 
 array1 = np.array(lista1)
 array2 = np.array(lista2)
-print(array1.shape, array2.shape)
+print("Para imprimir la forma del array:", array1.shape, array2.shape)
 
-print('suma de listas:', lista1 + lista2)
+print('\nsuma de listas:', lista1 + lista2)
 print('suma de arrays:', array1 + array2)
 
 print('la suma de array TIENE QUE tener la misma forma')
-print('resta de arrays:')
+print('\nresta de arrays:')
 print('resta', np.array([1, 2, 3])-np.array([5,6,1]))
 
 # Producto
@@ -24,17 +24,17 @@ print('resta', np.array([1, 2, 3])-np.array([5,6,1]))
 arr_1 = np.array([12, 14, 16, 18, 30])
 arr_2 = np.array([3, 7, 12, 2, 10])
 
-print('division por array', arr_1/arr_2)
+print('\ndivision por array', arr_1/arr_2)
 
 print('division por constante', arr_1/2)
 
 print('divsion constante sobre array', 10/arr_1)
 
-print('potencia de array', arr_2**2)
+print('\npotencia de array', arr_2**2)
 
-print('raiz de array', arr_2**(0.5))
+print('\nraiz de array', arr_2**(0.5))
 
-print('potencia de array con "numeros negativos"', 1/arr_2**2)
+print('\npotencia de array con "numeros negativos"', 1/arr_2**2)
 
 #potencia entre arreglos
 #un arreglo elevado a otra potencia
---
+++ pythonProject/Clases/clases_con_numpy/clase2.py
@@ -0,0 +1,90 @@
+# día: 17 de septiembre
+import numpy as np
+
+arr_2d = np.array(([0,5,10], [15,20,25], [30,35,40]))
+
+print(arr_2d)
+print("Forma:", arr_2d.shape)
+print("Dimensiones:",arr_2d.ndim)
+
+# Si tenemos dos dimensiones, necesitamos dos índices, uno para las filas
+# y otro para las columnas
+
+#primera fila
+primerafila = arr_2d[2]
+print(primerafila)
+# primera fila y primera columna
+primerafilaycolumna = arr_2d[2][1]
+print(primerafilaycolumna)
+# última fila y última columna
+ultimafilaycolumna = arr_2d[-1][-1]
+print(ultimafilaycolumna)
+# edición de la primera columna en la última fila
+arr_2d[-1][0] = 99 #aquí cambiamos un valor
+
+#usando slicing
+x = arr_2d[:, :]
+print(x)
+
+#subarray de las dos primeras filas
+a = arr_2d[:2,:]
+print("\ndos primeras filas\n", a)
+
+#subarray de la primera columna
+b = arr_2d[:,:1]
+print("\nprimera columna\n", b)
+
+#subarray de la primera fila, primera columna
+c = arr_2d[1:,:1]
+print("primera columna desde 1\n", c)
+
+#Edición masiva de la segunda columna
+arr_2d[:,1:2] = 99
+print(arr_2d)
+
+#edición masiva de la última fila
+arr_2d[-1,:] = [88,200,203]
+print(arr_2d)
+
+#Fancy index
+#se basa en pasarle una lista al array haciendo referencia a las filas
+#donde queremos acceder
+
+#Creamos un array 2d de ceros
+arr_2d = np.zeros((5,10))
+print(arr_2d)
+
+#modificacion masiva de la primera, tercer y última fila
+arr_2d[[0,2,-1]] = 5
+print(arr_2d)
+
+#usando bucles for
+#podemos recorrer las filas de un array con un bucle for como si se tratase de una lista
+
+for fila in arr_2d:
+    print("usando for", fila)
+
+for i, fila in enumerate(arr_2d):
+    for j, columna in enumerate(fila):
+        arr_2d[i][j] = len(fila) * i + j
+
+print(arr_2d)
+
+#EJERCICIOS
+arr_2d = np.zeros((5,10))
+
+for i, fila in enumerate(arr_2d):
+    for j, columna in enumerate(fila):
+        arr_2d[i][j] = len(fila) * i + j
+
+#Ejercicio 1: Determine la dimensión y forma de la matriz arr_2d
+print("forma:", arr_2d.shape)
+print("Dimensión:",arr_2d.ndim)
+#Ejercicio 2: Transponga la matriz
+print('la transpuesta de la matriz es:\n', arr_2d.T)
+#Ejercicio 3: Modifique los valores de la 5 columna por 999
+arr_2d[:,5] = 999
+print(arr_2d)
+#Ejercicio 4: Modifique los valores de las filas 3 y 4 por -5
+arr_2d[2:4, :] = -5
+print(arr_2d)
